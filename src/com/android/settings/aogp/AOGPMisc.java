8368021e995f0dcf606cb5f1965cf8214cd11067
Jmz <jmztaylor@gmail.com>
2014-06-21 15:04:31 -0400

    Disable Immersive Messages [1/2]
    Change-Id: I63ca47f9eecf8e3dcfc0b5b0c6cdb70b7c1a0d6c

--- src/com/android/settings/aogp/AOGPMisc.java
+++ src/com/android/settings/aogp/AOGPMisc.java
@@ -16,24 +16,58 @@
 
 package com.android.settings.aogp;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
 import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 import java.util.List;
+import java.util.ArrayList;
 
-public class AOGPMisc extends SettingsPreferenceFragment {
-
+public class AOGPMisc extends SettingsPreferenceFragment implements
+         Preference.OnPreferenceChangeListener {
+	private static final String TAG = "AOGP Misc";		 
+			 
+    private static final String DISABLE_IMMERSIVE_MESSAGE = "disable_immersive_message";
+    
+    private CheckBoxPreference mDisableIM;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.aogp_misc);
+        
+    PreferenceScreen prefSet = getPreferenceScreen();    
+    
+    mDisableIM = (CheckBoxPreference) findPreference(DISABLE_IMMERSIVE_MESSAGE);
+    mDisableIM.setChecked((Settings.System.getInt(resolver,
+            Settings.System.DISABLE_IMMERSIVE_MESSAGE, 0) == 1));
+            
     }
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if  (preference == mDisableIM) {
+            boolean checked = ((CheckBoxPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.DISABLE_IMMERSIVE_MESSAGE, checked ? 1:0);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }            
 }
